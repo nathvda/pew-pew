b15af844cf4d9f06c8455eea2fd80955de7336eb
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-12-30 14:52:20 +0100

    Changes controllers

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 let canvas = document.getElementById("game");
 let dx = 25;
 
@@ -7,6 +5,13 @@ canvas.setAttribute("width",`${window.innerWidth}px`);
 canvas.setAttribute("height",`${window.innerHeight}px`);
 canvas.style.border = "8px solid black";
 canvas.style.background = "black";
+
+var ctx = canvas.getContext("2d");
+
+let x = window.innerWidth/2;
+let y = window.innerHeight-80;
+let pWidth;
+let posBu;
 
 class Bullet{
     constructor(canvas,x,y,velocity, bulletColor){
@@ -40,6 +45,28 @@ class Player{
 
         this.height = 40;
         this.width = this.height * 2; 
+
+        document.addEventListener("keydown", (e) => {
+            console.log("duh");
+            if (e.code == "ArrowRight" && (player.x + pWidth < innerWidth)){
+
+                if (player.x + pWidth > innerWidth){
+        
+                } else {
+                    player.x += dx;
+                    posBu = player.x;
+                }
+            } else if (e.code == "ArrowLeft" && player.x > 0){
+                
+                if (player.x < 0){
+                } else {
+                    player.x -= dx;
+                    posBu = player.x;
+                }
+            } else if (e.code == "Space"){
+                player.shoot();
+            }
+        })
     }
     
     draw(ctx){
@@ -48,50 +75,20 @@ class Player{
     }
 
     shoot(){
-        console.log("bullet created");
-        const bullet = new Bullet(canvas, posBu,this.y, 10, "white");
+        const bullet = new Bullet(canvas, this.x ,this.y, this.velocity, "white");
         bullet.draw(ctx);
+        console.log("shoot");
     }
 }
-
-
-document.addEventListener("keydown", (e) => {
-
-    if (e.code == "ArrowRight" && (player.x + pWidth < innerWidth)){
-
-        if (player.x + pWidth > innerWidth){
-
-        } else {
-            player.x += dx;
-            posBu = player.x;
-        }
-    } else if (e.code == "ArrowLeft" && player.x > 0){
-        
-        if (player.x < 0){
-        } else {
-            player.x -= dx;
-        }
-    } else if (e.code == "Space"){
-        player.shoot();
-    }
-
-})
-
-var ctx = canvas.getContext("2d");
-
-let x = window.innerWidth/2;
-let y = window.innerHeight-80;
-let pWidth;
-let posBu;
 
 const player = new Player(canvas, x, y, dx, "red");
 
 function animateCanvas(){
-   ctx.clearRect(0,0, innerWidth, innerHeight);
 
    requestAnimationFrame(animateCanvas);
+   ctx.clearRect(0,0, innerWidth, innerHeight);
    pWidth = player.width;
-   player.draw(ctx);
+   player.draw(ctx); 
 }
 
 animateCanvas();
